d24153962ed50b1d85192f5323fd7156c9bfd319
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-18 22:31:53 +0200

    changed variable

--- ASCII.py
+++ ASCII.py
@@ -1,5 +1,5 @@
 
 #show ASCII number from button
-c = input("Enter only one value: ")
+value = input("Enter only one value: ")
 # print the ASCII value of assigned character in c
-print("The ASCII value of '" + c + "' is", ord(c))
+print("The ASCII value of '" + value + "' is", ord(value))
